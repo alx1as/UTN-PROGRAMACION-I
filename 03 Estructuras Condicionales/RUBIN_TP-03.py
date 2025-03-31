b172efb337584e6fc04bad63e1acfc74910255b0
Alx1as <alexiarubin23@gmail.com>
2025-03-31 20:51:02 -0300

    agregado sección de adulto punto 4

--- 03 Estructuras Condicionales/RUBIN_TP-03.py
+++ 03 Estructuras Condicionales/RUBIN_TP-03.py
@@ -21,7 +21,7 @@ if nro % 2 == 0:
 else:
     print("Por favor, ingrese un número par: ")
 
-#4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece: Niño/a: menor de 12 años.  Adolescente: mayor o igual que 12 años y menor que 18 años. Adulto/a joven: mayor o igual que 18 años y menor que 30 años. Adulto/a: mayor o igual que 30 años."""
+#4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece: Niño/a: menor de 12 años.  Adolescente: mayor o igual que 12 años y menor que 18 años. Adulto/a joven: mayor o igual que 18 años y menor que 30 años. Adulto/a: mayor o igual que 30 años. 
  
 edad=int(input("Por favor ingrese su edad: "))
 if edad < 12:
@@ -30,5 +30,7 @@ elif edad>=12 and edad<18:
     print("Eres un adolescente")
 elif edad>=18 and edad<30:
     print("Eres un adulto jóven")
-else:
+elif edad>=30:
     print("Eres un adulto")
+
+#5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres(incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal como una lista o un string."""
