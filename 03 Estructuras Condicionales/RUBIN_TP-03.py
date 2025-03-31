b833312f8677aff5388d47440181874590669284
Alx1as <alexiarubin23@gmail.com>
2025-03-31 15:02:26 -0300

    #1 resuelto

---
+++ 03 Estructuras Condicionales/RUBIN_TP-03.py
@@ -0,0 +1,8 @@
+#1) Solicitar la edad del usuario. Si el usuario es mayor de 18 años, mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+edad = int(input("Por favor, ingrese su edad: "))
+if edad > 18:
+    print("Es mayor de edad")
+else:
+    pass
+
+#2 
