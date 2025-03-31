298950e80634c8108750f145530e63cc23b0529e
Alx1as <alexiarubin23@gmail.com>
2025-03-31 16:03:36 -0300

    #4) resuelto

--- 03 Estructuras Condicionales/RUBIN_TP-03.py
+++ 03 Estructuras Condicionales/RUBIN_TP-03.py
@@ -11,7 +11,7 @@ nota = int(input("Ingrese su nota: "))
 if nota>= 6:
     print("Aprobado")
 else:
-    print("Desaprobado")"""
+    print("Desaprobado")
 
 #3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del operador de módulo (%) en Python para evaluar si un número es par o impar.
 
@@ -21,5 +21,14 @@ if nro % 2 == 0:
 else:
     print("Por favor, ingrese un número par: ")
 
-#4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece: Niño/a: menor de 12 años.  Adolescente: mayor o igual que 12 años y menor que 18 años. Adulto/a joven: mayor o igual que 18 años y menor que 30 años. Adulto/a: mayor o igual que 30 años.
+#4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece: Niño/a: menor de 12 años.  Adolescente: mayor o igual que 12 años y menor que 18 años. Adulto/a joven: mayor o igual que 18 años y menor que 30 años. Adulto/a: mayor o igual que 30 años."""
  
+edad=int(input("Por favor ingrese su edad: "))
+if edad < 12:
+    print("Eres un niño")
+elif edad>=12 and edad<18:
+    print("Eres un adolescente")
+elif edad>=18 and edad<30:
+    print("Eres un adulto jóven")
+else:
+    print("Eres un adulto")
