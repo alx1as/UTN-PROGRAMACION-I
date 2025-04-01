54f84288ee97f35f54196f38d60e12fff195def7
Alx1as <alexiarubin23@gmail.com>
2025-04-01 13:54:41 -0300

    punto 7 resuelto

--- 03 Estructuras Condicionales/RUBIN_TP-03.py
+++ 03 Estructuras Condicionales/RUBIN_TP-03.py
@@ -67,3 +67,8 @@ print(f"Moda: {moda}")
 #7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por pantalla."""
 
 palabra = input("Escribe una palabra o frase: ")
+ultima_letra= palabra[-1] #indexación negativa para tomar última letra.
+if ultima_letra == "a" or ultima_letra == "e" or ultima_letra == "i" or ultima_letra == "o" or ultima_letra == "u":
+    print(f"{palabra}!")
+else:
+    print(f"{palabra}")
