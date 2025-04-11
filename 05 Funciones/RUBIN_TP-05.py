8345a549f0dac56ab8153f4073728437906dd003
Alx1as <alexiarubin23@gmail.com>
2025-04-11 19:29:56 -0300

    9

--- 05 Funciones/RUBIN_TP-05.py
+++ 05 Funciones/RUBIN_TP-05.py
@@ -85,7 +85,7 @@ n1=int(input("Ingrese un número: "))
 n2=int(input("Ingrese un número: "))
 operaciones_basicas(n1,n2)
 
-#8) Crear una función llamada calcular_imc(peso, altura) que reciba el peso en kilogramos y la altura en metros, y devuelva el índice de masa corporal (IMC). Solicitar al usuario los datos y llamar a la función para mostrar el resultado con dos decimales."""
+#8) Crear una función llamada calcular_imc(peso, altura) que reciba el peso en kilogramos y la altura en metros, y devuelva el índice de masa corporal (IMC). Solicitar al usuario los datos y llamar a la función para mostrar el resultado con dos decimales.
 
 def calcular_imc(peso,altura):
     imc=peso/(altura**2)
@@ -94,3 +94,13 @@ def calcular_imc(peso,altura):
 peso=float(input("Ingrese su peso en kg: "))
 altura=float(input("Ingrese su altura en m²: "))
 calcular_imc(peso,altura)
+
+#9. Crear una función llamada celsius_a_fahrenheit(celsius) que reciba una temperatura en grados Celsius y devuelva su equivalente en Fahrenheit. Pedir al usuario la temperatura en Celsius y mostrar el resultado usando la función."""
+def celsius_a_fahrenheit(celsius):
+    fahrenheit=(celsius*9)/5+32
+    return fahrenheit
+
+#principal
+celsius=float(input("Ingrese la temperatura en grados celsius: "))
+fahrenheit = celsius_a_fahrenheit(celsius)
+print(f"Los grados ingresados corresponden a {fahrenheit}° Fahrenheit")
