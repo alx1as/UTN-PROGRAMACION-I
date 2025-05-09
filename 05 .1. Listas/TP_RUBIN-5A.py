dfcc5791d951305a730a1fc21a67285bdc9b7314
Alx1as <alexiarubin23@gmail.com>
2025-05-09 09:47:50 -0300

    Empezando tp 5.

---
+++ 05 .1. Listas/TP_RUBIN-5A.py
@@ -0,0 +1,11 @@
+"""#1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
+for i in list(range(4,101,4)):
+        print(i)
+    #Acá me doy cuenta que con lista probablemente se refiera a un array asi que voy a hacerlo de nuevo:
+multiplos_4 = list(range(4,101,4))
+print(multiplos_4)"""
+
+#2) Crear una lista con 5 elementos y mostrar el penúltimo.
+elementos=["mesa", "cubo", "perro", "palta", "música"]
+print(elementos[3])
+  
